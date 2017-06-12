08493d3726a9c8afed8f20288822dfdaaa22bce9
oscar luna <os.lun11@gmail.com>
2017-06-11 19:44:29 -0700

    updated

--- server.js
+++ server.js
@@ -1,47 +1,100 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+// dependencies 
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var Article = require(".model/Article.js");
+var Article = require('./models/Article.js');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
-
+var PORT = process.env.PORT || 8080;
+var path = require('path');
 // Run Morgan for Logging
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({type: "application/vnd.api+json"}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use(express.static("./public"));
+app.use('/public', express.static('./public'));
 
-mongoose.connect("mongodb://localhost/nytreact");
+// mongo db connection here 
 
-//link to heroku
+mongoose.connect('mongodb://localhost/nytreact');
 
 var db = mongoose.connection;
 
-db.on("error", function (err) {
+// var uri = "mongodb://;
+
+// // heroku connect
+// mongoose.connect(uri);
+
+
+db.on("error", function(err){
+  console.log("Mongoosey error: ", err);
+});
+
+db.once("open", function(){
+  console.log("Mongoose connection success my friend "  );
 
 });
 
-db.once("open", function () {
-	console.log("Mongoose connection successful.");
+app.get("/", function(req, res){
+  console.log(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
+
+})
+
+app.get("/api/saved", function(req, res){
+  
+  Article.find({})
+  .exec(function(err, doc){
+    if(err){
+      console.log(err);
+
+    }
+    else {
+      res.send(doc);
+    }
+  })
 });
 
-//Routes
-app.get("/", function(req, res) {
-	res.sendFile("./public/index.html");
+app.post('/api/saved', function(req, res){
+
+  var newArticle = new Article({
+    title: req.body.title,
+    date: req.body.date,
+    url: req.body.url
+  });
+
+  newArticle.save(function(err, doc){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(doc);
+    }
+  });
+
 });
 
-// app.get("/api/saved", function(req, res) {
+app.delete('/api/saved/:id', function(req, res){
 
-// });
+  Article.find({'_id': req.params.id}).remove()
+    .exec(function(err, doc) {
+      res.send(doc);
+  });
+
+});
 
 
 app.listen(PORT, function() {
-	console.log("App Listening on PORT: " + PORT);
+  console.log("App listening on PORT 8080 Davey: " + PORT);
 });
+
+
+
+
 
 
 
